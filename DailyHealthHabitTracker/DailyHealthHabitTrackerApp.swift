74c91fb1f1e5fa87f3672ab975cee691181482bd
Drew Brokamp <drew.brokamp@gmail.comm>
2022-02-13 00:27:25 -0500

    Initial Commit

---
+++ DailyHealthHabitTracker/DailyHealthHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyHealthHabitTrackerApp.swift
+//  DailyHealthHabitTracker
+//
+//  Created by Drew Brokamp on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyHealthHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
