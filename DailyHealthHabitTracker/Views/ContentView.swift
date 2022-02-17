a1f759785ac6de2f8f76b15a7cb651411b34facb
Drew Brokamp <drew.brokamp@gmail.comm>
2022-02-16 19:40:34 -0500

    Updated main view font to title3

--- DailyHealthHabitTracker/Views/ContentView.swift
+++ DailyHealthHabitTracker/Views/ContentView.swift
@@ -27,12 +27,14 @@ struct ContentView: View {
             HStack {
                 Text("Journal")
                     .padding()
+                    .font(.title3)
                 Spacer()
                 Button(action: {
                     journalIsSheetPresented.toggle()
                 }, label: {
                     Image(systemName: "plus")
                         .padding()
+                        .font(.title3)
                 })
                     .sheet(isPresented: $journalIsSheetPresented, onDismiss: {}, content: { NewJournalEntry(today: $today, journalIsSheetPresented: $journalIsSheetPresented) })
 
@@ -40,23 +42,29 @@ struct ContentView: View {
             HStack {
                 Text("Medication Schedule")
                     .padding()
+                    .font(.title3)
                 Spacer()
                 Image(systemName: "plus")
                     .padding()
+                    .font(.title3)
             }
             HStack {
                 Text("Workout")
                     .padding()
+                    .font(.title3)
                 Spacer()
                 Image(systemName: "plus")
                     .padding()
+                    .font(.title3)
             }
             HStack {
                 Text("Daily Gratitude")
                     .padding()
+                    .font(.title3)
                 Spacer()
                 Image(systemName: "plus")
                     .padding()
+                    .font(.title3)
             }
             Spacer()
         }
